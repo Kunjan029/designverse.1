3b0a86d1a534087a3eea7e3f5a4e27c98176632c
Kunjan029 <47239616+Kunjan029@users.noreply.github.com>
2026-07-01 16:33:02 +0530

    Update DesignverseRoot.tsx

--- components/core/DesignverseRoot.tsx
+++ components/core/DesignverseRoot.tsx
@@ -2,11 +2,13 @@
 
 import { MotionConfigProvider } from "@/components/shared/MotionConfigProvider";
 import AtmosphereLayer from "@/components/core/AtmosphereLayer";
+import PersistentNav from "@/components/core/PersistentNav";
 
 export default function DesignverseRoot() {
   return (
     <MotionConfigProvider>
       <AtmosphereLayer />
+      <PersistentNav />
       <div>Hello Designverse</div>
     </MotionConfigProvider>
   );
