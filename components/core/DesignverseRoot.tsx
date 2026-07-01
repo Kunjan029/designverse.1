6aa6711b62099eddde0652d85a33aec654cc0146
Kunjan029 <47239616+Kunjan029@users.noreply.github.com>
2026-07-01 16:31:24 +0530

    Update DesignverseRoot.tsx

--- components/core/DesignverseRoot.tsx
+++ components/core/DesignverseRoot.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { MotionConfigProvider } from "@/components/shared/MotionConfigProvider";
+import AtmosphereLayer from "@/components/core/AtmosphereLayer";
 
 export default function DesignverseRoot() {
   return (
     <MotionConfigProvider>
+      <AtmosphereLayer />
       <div>Hello Designverse</div>
     </MotionConfigProvider>
   );
